777c5368bec5d987a61efa6030c31d745db00974
GEOVANE MELLO GONZATTO <geovane_gonzatto@estudante.sesisenai.org.br>
2024-08-26 20:29:55 -0300

    first commit

--- Semana02/src/index.ts
+++ Semana02/src/index.ts
@@ -1,54 +1,10 @@
-import leia from 'readline-sync'
+import Cachorro from "./Cachorro";
 
-/*Escreva um algoritmo que receba 5 nomes e 5 idades do usuário, armazene essas informações em dois vetores,
- e depois exiba o nome e a idade da pessoa mais velha e da pessoa mais nova.*?*/
+var c1 = new Cachorro();
 
- var idades: number [] = [];
- var nomes: string [] = [];
+c1.nome = "Bento"
+c1.raca = "Vagabundo"
+c1.cor = "Preto"
 
- var nomeNova: string;
- var IdadeNova: number;
-
-  var nomevelha: string;
- var idadevelha: number;
- 
-
-
-function pedirInfoUsuario(qts: number){
-    for( var i= 0; i<qts; i++){
-        var nome = leia.question("Informe o Nome: ");
-        nomes.push(nome);
-        var idade = leia.questionInt("Informe a Idade: ");
-        idades.push(idade);
-
-    }
-}
-
-function PessoasMaisNovas(){ //rever e ajustar
-    for(var i =0; i<idades.length; i++){
-if(IdadeNova === undefined || idades[i]<IdadeNova){
-    IdadeNova = idades[i];
-    nomeNova = nomes[i];
-    console.log(` PESSOA MAIS VELHA É ${nomeNova} com ${IdadeNova} ANOS`);
-}
-
-    }
-
-}
-
-function PessoasMaisVelhas (){
-idades.forEach((idade,index) => {
-    if(idadevelha === undefined || idades[index]<idadevelha){
-        idadevelha = idades[index];
-        nomevelha = nomes[index];
-        console.log(`A PESSOA MAIS VELHA É ${nomevelha} com ${idadevelha} ANOS`);
-}})
-}
-
-pedirInfoUsuario (5);
-PessoasMaisNovas ();
-PessoasMaisVelhas();
-
-
-
+console.log (c1.nome)
 
